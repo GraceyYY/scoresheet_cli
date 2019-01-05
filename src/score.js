072b54d1097878159a3ab6e4b6308cd0ad0f9ae1
Grace <lunay0325@gmail.com>
2019-01-05 17:33:02 +0800

    Create the score class

---
+++ src/score.js
@@ -0,0 +1,19 @@
+class Score {
+  constructor(scores) {
+    this.scores = scores;
+  }
+
+  getTotalScore() {
+    return [...this.scores.values()].reduce((sum, value) => {
+      return sum + value;
+    });
+  }
+
+  getAverageScore() {
+    return [...this.scores.values()].reduce((avr, value, index, arr) => {
+      return index < arr.length - 1 ? avr + value : (avr + value) / arr.length;
+    }, 0);
+  }
+};
+
+module.exports = Score;
