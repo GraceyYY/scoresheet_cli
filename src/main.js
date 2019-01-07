d3308db0f77e91615efaecb5388096c69c62d1e3
Grace <lunay0325@gmail.com>
2019-01-07 15:27:55 +0800

    Complete checkStudentId function

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ module.exports = () => {
   import Score from 'score.js';
   import Scoresheet from 'scoresheet.js';
 
+  const currentStudents = new Map();
+
   function isInputValid(input) {
     const reg = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
     return input.match(reg) ? true : false;
+  }
+
+  function checkStudentId(id) {
+    return currentStudents.has(id);
   }
 }
