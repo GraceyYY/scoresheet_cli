6334e1777cefe23529d0891a5ccb0c234c0d600f
Grace <lunay0325@gmail.com>
2019-01-07 15:22:24 +0800

    Complete isInputValid function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 module.exports = () => {
+  import Student from 'student.js';
+  import Score from 'score.js';
+  import Scoresheet from 'scoresheet.js';
 
+  function isInputValid(input) {
+    const reg = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
+    return input.match(reg) ? true : false;
+  }
 }
