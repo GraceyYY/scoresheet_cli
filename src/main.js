2cb0b64b11f634ae6dab53346320fdc78b49f2e7
Grace <lunay0325@gmail.com>
2019-01-07 16:39:55 +0800

    Replace the import syntax to require syntax to import a module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 module.exports = () => {
-  import Student from 'student.js';
-  import Score from 'score.js';
-  import Scoresheet from 'scoresheet.js';
+  const Student = require('./student.js');
+  const Score = require('./score.js');
   const currentStudents = new Map();
   const regExp = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
 
@@ -14,11 +13,10 @@ module.exports = () => {
   }
 
   function addScores(input) {
-    const processedInput = input.match(regExp)[0].split('，')；
+    const processedInput = input.match(regExp)[0].split('，');
     const name = processedInput[0];
     const id = processedInput[1];
     const scoresArr = processedInput.slice(2).map(item => item.split('：'));
-
     if (!currentStudents.has(id)) {
       let score = new Score(new Map(scoresArr));
       let student = new Student(name, id, score);
