496c3201dfb948b191e619592940c7b37d3c9297
Grace <lunay0325@gmail.com>
2019-01-07 20:02:19 +0800

    Modify the output format

--- src/main.js
+++ src/main.js
@@ -11,8 +11,7 @@ module.exports = () => {
     1. 添加学生
     2. 生成成绩单
     3. 退出
-    请输入你的选择（1～3）：
-    `;
+    请输入你的选择（1～3）：`;
     let select = readlineSync.question(menu);
     switch (select) {
       case '1':
