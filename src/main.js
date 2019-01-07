1907f3fa3ca522826d42fe4e2da3e3892b6f55df
Grace <lunay0325@gmail.com>
2019-01-07 16:03:44 +0800

    Complete addScores function

--- src/main.js
+++ src/main.js
@@ -2,15 +2,32 @@ module.exports = () => {
   import Student from 'student.js';
   import Score from 'score.js';
   import Scoresheet from 'scoresheet.js';
-
   const currentStudents = new Map();
+  const regExp = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
 
   function isInputValid(input) {
-    const reg = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
-    return input.match(reg) ? true : false;
+    return input.match(regExp) ? true : false;
   }
 
   function checkStudentId(id) {
     return currentStudents.has(id);
+  }
+
+  function addScores(input) {
+    const processedInput = input.match(regExp)[0].split('，')；
+    const name = processedInput[0];
+    const id = processedInput[1];
+    const scoresArr = processedInput.slice(2).map(item => item.split('：'));
+
+    if (!currentStudents.has(id)) {
+      let score = new Score(new Map(scoresArr));
+      let student = new Student(name, id, score);
+      currentStudents.set(id, student);
+    } else {
+      let student = currentStudents.get(id);
+      for (let [subject, grade] of scoresArr) {
+        student.score.set(subject, grade);
+      }
+    }
   }
 }
