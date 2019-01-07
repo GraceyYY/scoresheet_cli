1b124972a3630b9d6ebe223aea8ebed5c8dcc2eb
Grace <lunay0325@gmail.com>
2019-01-07 17:00:57 +0800

    Complete showMenu function

--- src/main.js
+++ src/main.js
@@ -28,4 +28,16 @@ module.exports = () => {
       }
     }
   }
+  const readlineSync = require('readline-sync');
+
+  function showMenu() {
+    const menu = `
+1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：
+`;
+    let select = readlineSync.question(menu);
+    return select;
+  }
 }
