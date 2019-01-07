0d0879d94ce13fe22d45d6489e83fbafacfa7925
Grace <lunay0325@gmail.com>
2019-01-07 17:54:53 +0800

    Modify regular expressions and functions related to judge wheter the input is valid

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 module.exports = () => {
   const Student = require('./student.js');
   const Score = require('./score.js');
+  const Scoresheet = require('./scoresheet.js');
   const currentStudents = new Map();
-  const regExp = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/g;
+  const studentRegExp = /^[\u4e00-\u9fa5]{2,4}，[0-9]{4}，([\u4e00-\u9fa5]{2}：[0-9]{1,2}[0]?，?)+$/gi;
+  const idRegExp = /^([0-9]{4}，?)+$/gi;
 
   function isInputValid(input) {
-    return input.match(regExp) ? true : false;
+    return input.match(studentRegExp) ? true : false;
   }
 
-  function checkStudentId(id) {
-    return currentStudents.has(id);
+  function isStudentIdValid(id) {
+    return id.match(idRegExp) ? true : false;
   }
 
   function addScores(input) {
-    const processedInput = input.match(regExp)[0].split('，');
+    const processedInput = input.split('，');
     const name = processedInput[0];
     const id = processedInput[1];
     const scoresArr = processedInput.slice(2).map(item => item.split('：'));
@@ -27,39 +29,29 @@ module.exports = () => {
         student.score.set(subject, grade);
       }
     }
+    return name;
   }
   const readlineSync = require('readline-sync');
 
-  function showMenu() {
-    const menu = `
-1. 添加学生
-2. 生成成绩单
-3. 退出
-请输入你的选择（1～3）：
-`;
-    let select = readlineSync.question(menu);
-    return select;
-  }
-
   function showHints(hint) {
     let msg = '';
-    switch(hint) {
-        case 'scoreFormat':
+    switch (hint) {
+      case 'scoreFormat':
         msg = '请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n';
         break;
-        case 'wrongScoreFormat':
+      case 'wrongScoreFormat':
         msg = '请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：\n';
         break;
-        case 'idFormat' :
+      case 'idFormat':
         msg = '请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n';
         break;
-        case 'wrongIdFormat' :
+      case 'wrongIdFormat':
         msg = '请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n';
         break;
-        default:
+      default:
         break;
-
-        return readlineSync.question(msg);
     }
+    return readlineSync.question(msg);
   }
+  mainMenu();
 }
